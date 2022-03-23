cc4e886c730dd1281238e6df64e6652a03c3bff2
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-03-22 19:42:53 -0500

    Update index.php
    adding database tests

--- index.php
+++ index.php
@@ -53,6 +53,36 @@ PRIMARY KEY (`Id`)
 printf("Table created\n");
 }
 
+
+//Create an Insert prepared statement and run it
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)"))
+{
+    mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+    mysqli_stmt_execute($stmt);
+    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+    mysqli_stmt_close($stmt);
+}
+
+//Run the Select query
+printf("Reading data from table: \n");
+$res = mysqli_query($conn, 'SELECT * FROM Products');
+while ($row = mysqli_fetch_assoc($res))
+ {
+    var_dump($row);
+ }
+
+
+//Run the Delete statement
+// $product_name = 'BrandNewProduct';
+// if ($stmt = mysqli_prepare($conn, "DELETE FROM Products WHERE ProductName = ?")) {
+// mysqli_stmt_bind_param($stmt, 's', $product_name);
+// mysqli_stmt_execute($stmt);
+// printf("Delete: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+// mysqli_stmt_close($stmt);
+// }
 ?>
 
 
