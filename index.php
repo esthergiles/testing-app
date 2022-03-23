3f856b24b9047206b09a417b84cc9fcfdd50ceb5
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-03-22 19:18:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,57 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-$con = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL); 
-mysqli_real_connect($conn, "demo-server.mysql.database.azure.com", "demoserveradmin", "sWq*75AMm", "demo", 3306, MYSQLI_CLIENT_SSL);
-?>
+    // Postback
+    $message = "";
+    if(isset($_POST['SubmitButton'])){ //check if form was submitted
+      $input = $_POST['inputText']; //get input text
+      $message = "Success! You entered: ".$input;
+    }   
+
+$host = 'lionsnap-server.mysql.database.azure.com';
+$username = 'taamhhvhmx';
+$password = 'E7HHMN348V848DA8*';
+$db_name = 'testconnect';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+// mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Lion Snap App</title>
+	<link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="content-body">
+            <div class="success-text">UNA SNAP Application UI</div>
+        </div>  
+
+        <form action="" method="post">
+        <?php echo $message; ?>
+        Enter your favorite color: 
+        <input type="text" name="inputText"/>
+        <input type="submit" name="SubmitButton"/>
+        </form> 
+    </div>
+
+    
 </body>
 </html>
